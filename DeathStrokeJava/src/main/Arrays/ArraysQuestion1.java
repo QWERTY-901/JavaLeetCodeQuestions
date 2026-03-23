e4cd661d59d34f9958091100a9fd520562c0073c
QWERTY-901 <qwertyattherate17@gmail.com>
2026-03-23 11:05:07 +0530

    the first sorting array class added

---
+++ DeathStrokeJava/src/main/Arrays/ArraysQuestion1.java
@@ -0,0 +1,16 @@
+package main.Arrays;
+
+import java.util.Arrays;
+
+//TO SORT THE ARRAY
+public class ArraysQuestion1 {
+	
+	public int[] sortedArray() {
+		int[] arr= {23, 67, 54, 11, 10, 9};
+		
+		 Arrays.sort(arr);
+		 
+		 return arr;
+	}
+
+}
---
+++ DeathStrokeJava/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.Arrays;
+
+import main.Arrays.ArraysQuestion1;
+
+public class Main {
+	
+	public static void main(String[] args) {
+//		System.out.println("hello");
+		ArraysQuestion1 aq = new ArraysQuestion1();
+		System.out.println("THE SORTED ARRAYS ARE AS FOLLOWS:");
+		System.out.println(Arrays.toString(aq.sortedArray()));
+	}
+
+}
